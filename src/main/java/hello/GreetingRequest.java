b3b5333c667416f2f032cfc35fce2fcd61400784
Amit Sethi <amit.sethi@hmcts.net>
2018-10-18 00:37:16 +0100

    committed

--- src/main/java/hello/GreetingRequest.java
+++ src/main/java/hello/GreetingRequest.java
@@ -1,18 +1,17 @@
 package hello;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by amitsethi on 17/10/2018.
  */
+@Data
+@Setter
+@Getter
 public class GreetingRequest {
 
     private String name;
 
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
